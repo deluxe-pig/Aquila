3fa8134dbff5eb33a14673d50933d1741ad251b8
byeo630 <byeo630@gmail.com>
2016-05-25 22:25:55 -0700

    (feat) Add 500 more exoPlanets

--- src/js/components/ExoPlanet.js
+++ src/js/components/ExoPlanet.js
@@ -2,7 +2,7 @@ import {Entity} from 'aframe-react';
 import React from 'react';
 
 export default props => (
-  <Entity geometry={`primitive: sphere; radius: 0.01`} material={{color: '#E66'}}
+  <Entity geometry={`primitive: sphere; radius: 0.05`} material={{color: '#E66'}}
                   position={`${props.exoPlanetData.x}, ${props.exoPlanetData.y}, ${props.exoPlanetData.z}`}
                   >
   </Entity>
