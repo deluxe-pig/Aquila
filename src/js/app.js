feec0944e0c3a20a9dc0fba816d050daecf1930d
byeo630 <byeo630@gmail.com>
2016-05-26 13:04:43 -0700

    (fix) Readd rotating box for reference

--- src/js/app.js
+++ src/js/app.js
@@ -31,13 +31,18 @@ class BoilerplateScene extends React.Component {
     return (
       <Scene fog="type: linear; color: #222; far: 100; near: 0">
         <Camera><Cursor/></Camera>
-
         <Sky/>
         {starData.map((star) => (<Star key={star.id} starData={star} />))}
         {exoPlanetData.map((exoPlanet) => (<ExoPlanet key={exoPlanet.id} exoPlanetData={exoPlanet} />))}
         <Entity light={{type: 'ambient', color: '#888'}}/>
         <Entity light={{type: 'directional', intensity: 0.5}} position={[-1, 1, 0]}/>
         <Entity light={{type: 'directional', intensity: 1}} position={[1, 1, 0]}/>
+
+        <Entity geometry="primitive: box" material={{color: this.state.color}}
+                onClick={this.changeColor}
+                position="0 0 -5">
+          <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
+        </Entity>
 
       </Scene>
     );
