2989a3eb1814d33c9d2d22f61fa8394b14b366c6
byeo630 <byeo630@gmail.com>
2016-05-25 22:26:17 -0700

    (feat) Make stars smaller

--- src/js/components/Star.js
+++ src/js/components/Star.js
@@ -2,7 +2,7 @@ import {Entity} from 'aframe-react';
 import React from 'react';
 
 export default props => (
-  <Entity geometry={`primitive: sphere; radius: ${props.starData.absmag / 15}`} material={{color: '#FFF'}}
+  <Entity geometry={`primitive: sphere; radius: ${props.starData.absmag / 60}`} material={{color: '#FFF'}}
                   position={`${props.starData.x}, ${props.starData.y}, ${props.starData.z}`}
                   >
   </Entity>
