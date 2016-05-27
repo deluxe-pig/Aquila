a724982414865306298fbd1c33e2da2d81120856
byeo630 <byeo630@gmail.com>
2016-05-27 01:03:31 -0700

    (refactor) Remove rotating cube

--- src/js/app.js
+++ src/js/app.js
@@ -20,13 +20,6 @@ class BoilerplateScene extends React.Component {
     }
   }
 
-  changeColor = () => {
-    const colors = ['red', 'orange', 'yellow', 'green', 'blue'];
-    this.setState({
-      color: colors[Math.floor(Math.random() * colors.length)],
-    });
-  };
-
   render () {
     return (
       <Scene fog="type: linear; color: #222; far: 100; near: 0">
@@ -37,12 +30,6 @@ class BoilerplateScene extends React.Component {
         <Entity light={{type: 'ambient', color: '#888'}}/>
         <Entity light={{type: 'directional', intensity: 0.5}} position={[-1, 1, 0]}/>
         <Entity light={{type: 'directional', intensity: 1}} position={[1, 1, 0]}/>
-
-        <Entity geometry="primitive: box" material={{color: this.state.color}}
-                onClick={this.changeColor}
-                position="0 0 -5">
-          <Animation attribute="rotation" dur="5000" repeat="indefinite" to="0 360 360"/>
-        </Entity>
 
       </Scene>
     );
